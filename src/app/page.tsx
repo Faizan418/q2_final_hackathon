730fe3d0d9cfb9d4f2838aebe73bf2cd317ce8b1
Muhammad Faizan Soomro <141871174+Faizan418@users.noreply.github.com>
2025-01-18 03:03:19 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Header from "../../components/Header/Header";
+
 import Section1 from "../../components/Section1/Section1";
 import Section2 from "../../components/Section2/Section2";
 import Section3 from "../../components/Section3/Section3";
